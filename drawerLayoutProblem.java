d2ddeee5c902858712916b1a7e1fe1b39ac32164
Tasnuva Tabassum oshin <tasnuva.oshin12@gmail.com>
2019-12-11 10:07:08 +0600

    drawer Layout header Problem
    Drawer Layout Header XML File Not working? Use this Java Code to Run this

---
+++ drawerLayoutProblem.java
@@ -0,0 +1,18 @@
+ navigationView = view.findViewById(R.id.nav_view);
+        View hView =  navigationView.inflateHeaderView(R.layout.header_layout);
+        ImageView imgvw = (ImageView)hView.findViewById(R.id.profile_image);
+        TextView tv = (TextView)hView.findViewById(R.id.nameText);
+        //imgvw .setImageResource();
+        tv.setText("oshin");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //if drawer header not working from xml try using this java code 
+        thanks 
